7e3fc5bf09654751d86646699697e692eb0c0876
Hesham Adel <hesham.adel@tajawal.com>
2018-07-22 00:18:41 +0200

    Implement the final endpoint

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const data = [{
-    "id": 424,
-    "caption": "Brazil",
-    "numberOfTeams": 25,
-    "numberOfGames": 8,
-}, {
-    "id": 433,
-    "caption": "Kupa na Bulgarija",
-    "numberOfTeams": 267,
-    "numberOfGames": 34,
-},{
-    "id": 444,
-    "caption": "European Championships France",
-    "numberOfTeams": 24,
-    "numberOfGames": 38,
-}];
 
 
 
 app
     .get('/v1/competitions', (req, res) => {
-        res.send(data);
+
+        res.send([{
+            "id": 424,
+            "caption": "Brazil",
+            "numberOfTeams": 25,
+            "numberOfGames": 8,
+        }, {
+            "id": 433,
+            "caption": "Kupa na Bulgarija",
+            "numberOfTeams": 267,
+            "numberOfGames": 34,
+        },{
+            "id": 444,
+            "caption": "European Championships France",
+            "numberOfTeams": 24,
+            "numberOfGames": 38,
+        }]);
+
     })
     .get('/v1/competitions/:competitionId/teams', (req, res) => {
-        const { competitionId } = req.params;
-        const resData = data.filter( (obj) => obj.id === parseInt(competitionId) );
-        res.send(resData);
+
+        res.send({
+            teams: [{
+                    "id": 4,
+                    "name": "Manchester",
+                    "crestUrl": "https://www.gstatic.com/webp/gallery3/1.png",
+                },
+                {
+                    "id": 3,
+                    "name": "ManCity",
+                    "crestUrl": "https://www.gstatic.com/webp/gallery3/2_webp_ll.png",
+                },
+                {
+                    "id": 7,
+                    "name": "team7",
+                    "crestUrl": "https://www.gstatic.com/webp/gallery3/3_webp_ll.png",
+                },
+                {
+                    "id": 6,
+                    "name": "Team8",
+                    "crestUrl": "https://www.gstatic.com/webp/gallery3/5_webp_ll.png",
+                }
+            ]
+        });
     })
-    .get('/v1/teams/:teamId/fixtures');
+    .get('/v1/teams/:teamId/fixtures', (req, res) => {
+        res.send({
+
+
+
+        });
+    });
 
 
 
